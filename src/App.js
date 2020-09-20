76315f2e9416a64f69745c02c0eb872708e5b094
Justin <justinms@outlook.com>
2020-09-20 12:16:16 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_USERS',
     });
   }
@@ -24,9 +24,11 @@ class App extends Component {
           <button onClick={() => this.handleOnClickItems()}>
             Click to change items count
             </button>
+
           <button onClick={() => this.handleOnClickUsers()}>
             Click to change user count
           </button>
+
           <p>{this.props.items.length}</p>
       </div>
     );
@@ -34,8 +36,10 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger
   return { items: state.items }
 }
 
+
 export default connect(mapStateToProps)(App);
+
